e588687ccaf1b5ab8a99dc9426a8a6acfbcc25f0
Jason Jernberg <jason.jernberg@gmail.com>
2014-03-05 15:59:11 -0600

    Options and comments
    Added default options and some additional comments.

--- jquery.popUpMenu.js
+++ jquery.popUpMenu.js
@@ -1,17 +1,32 @@
+/*
+ *  jQuery Popup Menu Plugin
+ */
+
 (function ($) {
-  $.fn.popupmenu = function (whatToPop, inSpeed, outSpeed, delayTime) {
+  $.fn.popupmenu = function (options) {
+    
+    // Add some default options. It will automatically find the first child element if one is not provided.
+    var settings = $.extend({
+      _what: $(this).next(),
+      _in: 'fast',
+      _out: 'fast',
+      _delay: 300
+    }, options );
+      
+    // Heart of the plugin. Watches for mouse actions on the element and acts accordingly.  
     $(this)
     .mouseenter(function() {
-      $(this).children(whatToPop).fadeIn(inSpeed);
+      $(this).children(settings._what).fadeIn(settings._in);
     })
     .mouseleave(function() {
-      $(this).children(whatToPop)
-      .delay(delayTime)
-      .fadeOut(outSpeed);
-      $(this).children(whatToPop)
+      $(this).children(settings._what)
+      .delay(settings._delay)
+      .fadeOut(settings._out);
+      $(this).children(settings._what)
       .mouseenter(function() {
         $(this).clearQueue();
       });
     });
+    return this;
   };
 }(jQuery));
